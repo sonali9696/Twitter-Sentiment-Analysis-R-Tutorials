aa414fd49d0bad4b5eae0b8a4ccbb495c0521ad2
Sonali Agrawal <sonali9696@gmail.com>
2016-12-17 13:07:43 +0530

    Update authentication.R

--- authentication.R
+++ authentication.R
@@ -1,12 +1,16 @@
+#make sure you have installed it first by: 
+#install.packages("twitteR") 
+#and
+#install.packages("ROAuth")
 library(ROAuth)
 library(twitteR)
 
 consumer_key <-"AKJsxNqX2D8uTo9orgjRirvWL"
 consumer_secret <- "QOKk0ctHhbXNQ5QaipqofrZQzWM92mfkcoP60xe7HJzjSUCz6F"
-access_token<-"2617540074-5l6gGJhCP8iw9DS7sVD9qsFaUGfWGO9fqlHt5Wg"
+access_token <- "2617540074-5l6gGJhCP8iw9DS7sVD9qsFaUGfWGO9fqlHt5Wg"
 access_secret <- "VVMfNIzgPEUmCk5QyIWr5A4ZSC2Lxy7CERoUtWs4jAe0l"
 
-#download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
+download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem") #downloads the certificate
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
@@ -17,3 +21,4 @@ cred <- OAuthFactory$new(consumerKey=consumer_key,
 	authURL='https://api.twitter.com/oauth/authorize')
 
 cred$handshake(cainfo="cacert.pem")
+#After this you are redirected to a URL automatically, click on Authorize App and enter the PIN generated there
