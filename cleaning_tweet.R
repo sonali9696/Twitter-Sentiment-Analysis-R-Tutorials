72940632707ec1245d86ecc8cbf646cdabdf5c91
Sonali Agrawal <sonali9696@gmail.com>
2016-12-23 19:51:29 +0530

    Update cleaning_tweet.R

--- cleaning_tweet.R
+++ cleaning_tweet.R
@@ -4,7 +4,8 @@ trump.tweets = searchTwitter("trump", n=1500)
 
 #converts to data frame
 df <- do.call("rbind", lapply(trump.tweets, as.data.frame))
+
 #remove odd characters
-df$text <- sapply(df$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
-df$text = gsub("(f|ht)tp(s?)://(.*)[.][a-z]+", "", df$text)
+df$text <- sapply(df$text,function(row) iconv(row, "latin1", "ASCII", sub="")) #remove emoticon
+df$text = gsub("(f|ht)tp(s?)://(.*)[.][a-z]+", "", df$text) #remove URL
 sample <- df$text
