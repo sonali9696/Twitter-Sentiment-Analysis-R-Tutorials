b8ca4817b235796e0dfe1a9da78d03c8eb7e6298
Sonali Agrawal <sonali9696@gmail.com>
2016-12-23 11:49:19 +0530

    Tutorial 2

---
+++ cleaning_tweet.R
@@ -0,0 +1,10 @@
+
+#Extract tweets. Example-
+trump.tweets = searchTwitter("trump", n=1500)  
+
+#converts to data frame
+df <- do.call("rbind", lapply(trump.tweets, as.data.frame))
+#remove odd characters
+df$text <- sapply(df$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
+df$text = gsub("(f|ht)tp(s?)://(.*)[.][a-z]+", "", df$text)
+sample <- df$text
